9a24d8ed5fb28a055b3328459cd911b1ffbce3de
Dmitry Leleko <dmitry.leleko@orionprotocol.io>
2023-05-18 18:04:06 +0300

    Minor fix

--- src/services/BlockchainService/schemas/index.ts
+++ src/services/BlockchainService/schemas/index.ts
@@ -2,3 +2,4 @@ export { default as historySchema } from './historySchema.js';
 export { default as infoSchema } from './infoSchema.js';
 export { default as cfdContractsSchema } from './cfdContractsSchema.js';
 export { default as cfdHistorySchema } from './cfdHistorySchema.js';
+export { default as governanceContractsSchema } from './governanceContractsSchema.js';
