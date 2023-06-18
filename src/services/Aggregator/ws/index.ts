f114ebd5c84f25a8b22a7a807497db2a807ad275
Oleg Nechiporenko <oleg.nechiporenko@booking.com>
2023-06-19 03:55:37 +0500

    feat: websocket transport

--- src/services/Aggregator/ws/index.ts
+++ src/services/Aggregator/ws/index.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import WebSocket from 'isomorphic-ws';
 import { validate as uuidValidate, v4 as uuidv4 } from 'uuid';
 import MessageType from './MessageType.js';
 import SubscriptionType from './SubscriptionType.js';
@@ -19,6 +18,7 @@ import type { fullOrderSchema, orderUpdateSchema } from './schemas/addressUpdate
 import cfdAddressUpdateSchema from './schemas/cfdAddressUpdateSchema.js';
 import futuresTradeInfoSchema from './schemas/futuresTradeInfoSchema.js';
 import { objectKeys } from '../../../utils/objectKeys.js';
+import { WebsocketTransport, type BufferLike, type WebsocketTransportEvents } from '../../WebsocketTransport.js';
 // import assertError from '../../../utils/assertError.js';
 // import errorSchema from './schemas/errorSchema';
 
@@ -135,24 +135,6 @@ const exclusiveSubscriptions = [
   SubscriptionType.ASSET_PAIRS_CONFIG_UPDATES_SUBSCRIBE,
 ] as const;
 
-type BufferLike =
-  | string
-  | Buffer
-  | DataView
-  | number
-  | ArrayBufferView
-  | Uint8Array
-  | ArrayBuffer
-  | SharedArrayBuffer
-  | readonly unknown[]
-  | readonly number[]
-  | { valueOf: () => ArrayBuffer }
-  | { valueOf: () => SharedArrayBuffer }
-  | { valueOf: () => Uint8Array }
-  | { valueOf: () => readonly number[] }
-  | { valueOf: () => string }
-  | { [Symbol.toPrimitive]: (hint: string) => string };
-
 const isSubType = (subType: string): subType is keyof Subscription => Object.values(SubscriptionType).some((t) => t === subType);
 
 const unknownMessageTypeRegex = /An unknown message type: '(.*)', json: (.*)/;
@@ -166,7 +148,7 @@ const nonExistentMessageRegex = /Could not cancel nonexistent subscription: (.*)
 const FUTURES_SUFFIX = 'USDF';
 
 class AggregatorWS {
-  private ws?: WebSocket | undefined;
+  private transport?: WebsocketTransport | undefined;
 
   // is used to make sure we do not need to renew ws subscription
   // we can not be sure that onclose event will recieve our code when we do `ws.close(4000)`
@@ -179,10 +161,6 @@ class AggregatorWS {
   }> = {};
 
   public onInit: (() => void) | undefined
-
-  public onWSOpen: ((event: WebSocket.Event) => void) | undefined
-
-  public onWSClose: ((event: WebSocket.CloseEvent) => void) | undefined
 
   public onError: ((err: string) => void) | undefined
 
@@ -217,30 +195,17 @@ class AggregatorWS {
     this.basicAuth = basicAuth
   }
 
-  private messageQueue: BufferLike[] = [];
-  private sendWsMessage(message: BufferLike) {
-    if (this.ws?.readyState === WebSocket.OPEN) {
-      this.ws.send(message);
-    } else {
-      this.messageQueue.push(message);
-    }
-  }
-
   private readonly handleWsOpen = () => {
-    for (const message of this.messageQueue) {
-      this.ws?.send(message);
-    }
-    this.messageQueue = [];
     this.setupHeartbeat();
   }
 
   private sendRaw(data: BufferLike) {
-    this.sendWsMessage(data);
+    this.transport?.sendMessage(data);
   }
 
   private send(jsonObject: Json) {
     const jsonData = JSON.stringify(jsonObject);
-    this.sendWsMessage(jsonData);
+    this.transport?.sendMessage(jsonData);
     this.logger?.(`Sent: ${jsonData}`);
   }
 
@@ -252,7 +217,7 @@ class AggregatorWS {
       } else {
         this.logger?.('Heartbeat timeout');
         this.isClosedIntentionally = true;
-        this.ws?.close(4000);
+        this.transport?.close(4000);
       }
     };
 
@@ -332,7 +297,7 @@ class AggregatorWS {
     //     });
     // } else makeSubscription();
 
-    if (!this.ws) {
+    if (!this.transport) {
       this.init();
       console.log(`Aggregator WS ${this.instanceId} is initialized`);
     }
@@ -408,8 +373,8 @@ class AggregatorWS {
 
   destroy() {
     this.isClosedIntentionally = true;
-    this.ws?.close();
-    delete this.ws;
+    this.transport?.close();
+    delete this.transport;
   }
 
   // private initPromise: Promise<void> | null = null;
@@ -429,21 +394,27 @@ class AggregatorWS {
   //   return this.initPromise;
   // }
 
+  public onWsOpen(openCallback: WebsocketTransportEvents['open']) {
+    return this.transport?.onOpen(openCallback);
+  }
+
+  public onWsClose(closeCallback: WebsocketTransportEvents['close']) {
+    return this.transport?.onClose(closeCallback);
+  }
+
   private init(isReconnect = false) {
     this.isClosedIntentionally = false;
-    this.ws = new WebSocket(this.api);
-    this.ws.onerror = (err) => {
+    this.transport = new WebsocketTransport(this.api);
+    this.transport.onError((err) => {
       this.onError?.(`AggregatorWS error: ${err.message}`);
       this.logger?.(`AggregatorWS: ${err.message}`);
-    };
-    this.ws.onclose = (event) => {
-      this.onWSClose?.(event);
+    });
+    this.transport.onClose(() => {
       this.logger?.(`AggregatorWS: connection closed ${this.isClosedIntentionally ? 'intentionally' : ''}`);
       this.clearHeartbeat();
       if (!this.isClosedIntentionally) this.init(true);
-    };
-    this.ws.onopen = (e) => {
-      this.onWSOpen?.(e);
+    });
+    this.transport.onOpen(() => {
       this.handleWsOpen();
       // Re-subscribe to all subscriptions
       if (isReconnect) {
@@ -461,8 +432,8 @@ class AggregatorWS {
           });
       }
       this.logger?.(`AggregatorWS: connection opened${isReconnect ? ' (reconnect)' : ''}`);
-    };
-    this.ws.onmessage = (e) => {
+    });
+    this.transport.onMessage((e) => {
       this.isAlive = true;
       const { data } = e;
       if (typeof data !== 'string') throw new Error('AggregatorWS: received non-string message');
@@ -688,7 +659,7 @@ class AggregatorWS {
         default:
           break;
       }
-    };
+    });
   }
 }
 
---
+++ src/services/WebsocketTransport.ts
@@ -0,0 +1,93 @@
+import type { ClientRequestArgs } from 'http';
+import WebSocket from 'isomorphic-ws';
+import type { Emitter } from 'nanoevents';
+import { createNanoEvents } from 'nanoevents';
+
+export type BufferLike =
+  | string
+  | Buffer
+  | DataView
+  | number
+  | ArrayBufferView
+  | Uint8Array
+  | ArrayBuffer
+  | SharedArrayBuffer
+  | readonly unknown[]
+  | readonly number[]
+  | { valueOf: () => ArrayBuffer }
+  | { valueOf: () => SharedArrayBuffer }
+  | { valueOf: () => Uint8Array }
+  | { valueOf: () => readonly number[] }
+  | { valueOf: () => string }
+  | { [Symbol.toPrimitive]: (hint: string) => string };
+
+export type WebsocketTransportEvents = {
+  message: (messageEvent: WebSocket.MessageEvent) => void
+  open: (openEvent: WebSocket.Event) => void
+  error: (error: WebSocket.ErrorEvent) => void
+  close: (closeEvent: WebSocket.CloseEvent) => void
+}
+
+export class WebsocketTransport {
+  private ws?: WebSocket | undefined;
+  private readonly emitter: Emitter<WebsocketTransportEvents>;
+
+  constructor(address: URL | string, options?: WebSocket.ClientOptions | ClientRequestArgs) {
+    this.ws = new WebSocket(address, options);
+    this.emitter = createNanoEvents<WebsocketTransportEvents>();
+    this.ws.onerror = (error) => {
+      this.emitter.emit('error', error);
+    }
+    this.ws.onmessage = (message) => {
+      this.emitter.emit('message', message);
+    }
+    this.ws.onclose = (closeEvent) => {
+      this.emitter.emit('close', closeEvent);
+    }
+    this.ws.onopen = (openEvent) => {
+      this.emitter.emit('open', openEvent);
+      this.flushMessageQueue();
+    }
+  }
+
+  onError(errorCallback: WebsocketTransportEvents['error']) {
+    return this.emitter.on('error', errorCallback);
+  }
+
+  onMessage(messageCallback: WebsocketTransportEvents['message']) {
+    return this.emitter.on('message', messageCallback);
+  }
+
+  private flushMessageQueue() {
+    for (const message of this.messageQueue) {
+      this.ws?.send(message);
+    }
+    this.messageQueue = [];
+  }
+
+  onClose(closeCallback: WebsocketTransportEvents['close']) {
+    return this.emitter.on('close', closeCallback)
+  }
+
+  onOpen(openCallback: WebsocketTransportEvents['open']) {
+    return this.emitter.on('open', openCallback);
+  }
+
+  private messageQueue: BufferLike[] = [];
+  sendMessage(message: BufferLike) {
+    if (this.ws?.readyState === WebSocket.OPEN) {
+      this.ws.send(message);
+    } else {
+      this.messageQueue.push(message);
+    }
+  }
+
+  close(code?: number, data?: Buffer) {
+    this.ws?.close(code, data);
+  }
+
+  destroy(code?: number, data?: Buffer) {
+    this.ws?.close(code, data);
+    delete this.ws;
+  }
+}
