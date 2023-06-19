0ede26dabd68944393d8a88de691f87238552ab9
Aleksandr Kraiz <selfsurfer@gmail.com>
2023-06-19 17:26:41 +0400

    examples

---
+++ examples/cancel-cfd-order.ts
@@ -0,0 +1,50 @@
+import {Electra, SignedCancelOrderRequest, SupportedChainId, crypt} from "../src/index";
+import {ethers} from "ethers";
+import {simpleFetch} from "simple-typed-fetch";
+
+const walletPrivateKey = "";
+
+const electra = new Electra({
+    referralAPI: '',
+    networks: {
+        97: {
+            chainId: SupportedChainId.BSC_TESTNET,
+            nodeJsonRpc: "https://test.electra.finance/bsc-testnet/rpc",
+            services: {
+                blockchainService: {
+                    http: "https://test.electra.finance/bsc-testnet",
+                },
+                aggregator: {
+                    http: "https://test.electra.finance/bsc-testnet/backend",
+                    ws: "https://test.electra.finance/bsc-testnet/v1",
+                },
+                priceFeed: {
+                    api: "https://test.electra.finance/bsc-testnet2/price-feed",
+                },
+            }
+        },
+    },
+});
+
+const unit = electra.getUnit(SupportedChainId.BSC_TESTNET);
+
+// Defining wallet
+const wallet = new ethers.Wallet(
+    walletPrivateKey,
+    unit.provider
+);
+
+const senderAddress = await wallet.getAddress();
+const orderIdToCancel = '0x000000...'; // orderId of the order you want to cancel
+
+const signedCancelOrderRequest: SignedCancelOrderRequest = await crypt.signCancelOrder(
+    senderAddress, // senderAddress
+    orderIdToCancel,
+    false, // usePersonalSign
+    wallet, // signer
+    unit.chainId,
+);
+
+// Cancel order
+const {orderId, remainingAmount} = await simpleFetch(unit.aggregator.cancelOrder)(signedCancelOrderRequest);
+console.log(`Order ${orderId} canceled. Remaining amount: ${remainingAmount}`);
