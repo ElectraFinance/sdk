a17a3e856a2c2248f256fb1c97278aafc13d6338
Aleksandr Kraiz <selfsurfer@gmail.com>
2023-07-12 10:53:39 +0400

    liquidation example improvements

--- examples/liquidation.ts
+++ examples/liquidation.ts
@@ -1,76 +1,97 @@
-import {ethers} from "ethers";
-import {simpleFetch} from "simple-typed-fetch";
-import {CrossMarginCFD__factory, ERC20__factory} from "@electra.finance/contracts/lib/ethers-v5";
+import 'dotenv/config';
 
-import { Electra, FuturesTradeInfo, SupportedChainId, crypt } from "../src";
-import { BigNumber} from "bignumber.js";
+import { ethers } from 'ethers';
+import { simpleFetch } from 'simple-typed-fetch';
+import { CrossMarginCFD__factory, ERC20__factory } from '@electra.finance/contracts/lib/ethers-v5/index.js';
 
-const walletPrivateKey = "";
+import type { FuturesTradeInfo } from '../src/index.js';
+import { Electra, SupportedChainId, crypt } from '../src/index.js';
+import { BigNumber } from 'bignumber.js';
 
-const hostURL = "https://test.electra.finance/bsc-testnet";
+// HOW TO EXECUTE THIS EXAMPLE
+
+// 1. Create a wallet with some BNB on BSC testnet or use an existing one
+// 2. Install dependencies: `npm i`
+// 3. Create a `.env` file with the following content:
+//    ```
+//    PRIVATE_KEY=YOUR_PRIVATE_KEY
+//    HOST=YOUR_HOST
+//    ```
+// 4. Run this example with `npm run examples:liquidation`
+
+const PRIVATE_KEY = process.env['PRIVATE_KEY'];
+const HOST = process.env['HOST'];
+
+if (PRIVATE_KEY === undefined) {
+  throw new Error('PRIVATE_KEY is not defined');
+}
+
+if (HOST === undefined) {
+  throw new Error('HOST is not defined');
+}
 
 const electra = new Electra({
-    marginMode: "cross",
-    referralAPI: '',
-    networks: {
-        97: {
-            chainId: SupportedChainId.BSC_TESTNET,
-            nodeJsonRpc: hostURL + "/rpc",
-            services: {
-                blockchainService: {
-                    http: hostURL,
-                },
-                aggregator: {
-                    http: hostURL + "/backend",
-                    ws: hostURL + "/v1",
-                },
-                priceFeed: {
-                    api: hostURL + "/price-feed",
-                },
-            }
+  marginMode: 'cross',
+  referralAPI: '',
+  networks: {
+    97: {
+      chainId: SupportedChainId.BSC_TESTNET,
+      nodeJsonRpc: HOST + '/rpc',
+      services: {
+        blockchainService: {
+          http: HOST,
         },
+        aggregator: {
+          http: HOST + '/backend',
+          ws: HOST + '/v1',
+        },
+        priceFeed: {
+          api: HOST + '/price-feed',
+        },
+      }
     },
+  },
 });
 
 const unit = electra.getUnit(SupportedChainId.BSC_TESTNET);
 
-const MARGIN_LEVEL_THRESHOLD = 80;
+// const MARGIN_LEVEL_THRESHOLD = 80;
 
 const getFuturesInfo = (walletAddress: string, instrument: string, amount: number) => {
-    return new Promise<FuturesTradeInfo>((resolve, reject) => {
-        const subId = unit.aggregator.ws.subscribe('fts', {
-            payload: {
-                s: walletAddress,
-                i: instrument,
-                a: amount
-            },
-            callback: data => {
-                unit.aggregator.ws.unsubscribe(subId);
-                unit.aggregator.ws.destroy();
-                resolve(data);
-            }
-        })
-        setTimeout(() => reject(new Error('Timeout')), 10000);
+  return new Promise<FuturesTradeInfo>((resolve, reject) => {
+    const subId = unit.aggregator.ws.subscribe('fts', {
+      payload: {
+        s: walletAddress,
+        i: instrument,
+        a: amount
+      },
+      callback: data => {
+        unit.aggregator.ws.unsubscribe(subId);
+        unit.aggregator.ws.destroy();
+        resolve(data);
+      }
     })
+    setTimeout(() => { reject(new Error('Timeout')); }, 10000);
+  })
 }
 
 const getPriceChange24h = (s: string) => {
-    return new Promise<number>((resolve, reject) => {
-        const { unsubscribe } = unit.priceFeed.ws.subscribe('ticker', {
-            payload: s,
-            callback: ({ openPrice, lastPrice }) => {
-                unsubscribe();
-                resolve(new BigNumber(openPrice).isZero()
-                ? 0
-                : new BigNumber(lastPrice)
-                    .div(openPrice)
-                    .minus(1)
-                    .multipliedBy(100)
-                    .toNumber());
-            }
-        })
-        setTimeout(() => reject(new Error('Timeout')), 10000);
+  return new Promise<number>((resolve, reject) => {
+    const { unsubscribe } = unit.priceFeed.ws.subscribe('ticker', {
+      payload: s,
+      callback: ({ openPrice, lastPrice }) => {
+        unsubscribe();
+        resolve(new BigNumber(openPrice).isZero()
+          ? 0
+          : new BigNumber(lastPrice)
+            .div(openPrice)
+            .minus(1)
+            .multipliedBy(100)
+            .toNumber());
+      }
     })
+    setTimeout(() => { reject(new Error('Timeout')); }, 10000);
+  })
 }
 
 // const getPositions = (walletAddress: string) => {
@@ -93,111 +114,116 @@ const getPriceChange24h = (s: string) => {
 // }
 
 const waitOrderSettlement = (address: string, orderId: string) => {
-    return new Promise<void>((resolve, reject) => {
-        const subId = unit.aggregator.ws.subscribe('aus', {
-            payload: address,
-            callback: data => {
-                if(data.kind === 'initial'){
-                    const order = data.orders?.find(o => o.id === orderId);
-                    if(order?.status === 'SETTLED'){
-                        unit.aggregator.ws.unsubscribe(subId);
-                        unit.aggregator.ws.destroy();
-                        resolve();
-                    }
-                } else {
-                    if (data.order && data.order.id === orderId && data.order.status === 'SETTLED') {
-                        unit.aggregator.ws.unsubscribe(subId);
-                        unit.aggregator.ws.destroy();
-                        resolve();
-                    }
-                }
-            }
-        })
-        setTimeout(() => reject(new Error('Timeout')), 30000);
+  return new Promise<void>((resolve, reject) => {
+    const subId = unit.aggregator.ws.subscribe('ausf', {
+      payload: address,
+      callback: data => {
+        if (data.kind === 'initial') {
+          const order = data.orders?.find(o => o.id === orderId);
+          if (order?.status === 'SETTLED') {
+            unit.aggregator.ws.unsubscribe(subId);
+            unit.aggregator.ws.destroy();
+            resolve();
+          }
+        } else {
+          if (data.order && data.order.id === orderId && data.order.status === 'SETTLED') {
+            unit.aggregator.ws.unsubscribe(subId);
+            unit.aggregator.ws.destroy();
+            resolve();
+          }
+        }
+      }
     })
+    setTimeout(() => {
+      reject(new Error('Order settlement timeout'));
+    }, 30000);
+  })
 }
 
 // Returns margin level
 const waitPositionOpen = (address: string, instrument: string) => {
-    return new Promise<number>((resolve, reject) => {
-        const subId = unit.aggregator.ws.subscribe('ausf', {
-            payload: address,
-            callback: data => {
-                const position = data.balances?.find(p => p.instrument === instrument);
-                if (position && parseFloat(position.position) !== 0){
-                    const marginLevel = new BigNumber(position.equity).div(position.marginUSD).multipliedBy(100).toNumber();
+  return new Promise<number>((resolve, reject) => {
+    const subId = unit.aggregator.ws.subscribe('ausf', {
+      payload: address,
+      callback: data => {
+        const position = data.balances?.find(p => p.instrument === instrument);
+        if (position && parseFloat(position.position) !== 0) {
+          const marginLevel = new BigNumber(position.equity).div(position.marginUSD).multipliedBy(100).toNumber();
 
-                    unit.aggregator.ws.unsubscribe(subId);
-                    unit.aggregator.ws.destroy();
-                    resolve(marginLevel);
-                }
-            }
-        })
-        setTimeout(() => reject(new Error('Timeout')), 30000);
+          unit.aggregator.ws.unsubscribe(subId);
+          unit.aggregator.ws.destroy();
+          resolve(marginLevel);
+        }
+      }
     })
+    setTimeout(() => {
+      reject(new Error('Position open timeout'));
+    }, 30000);
+  })
 }
 
 const waitLiquidationOrder = (address: string, instrument: string) => {
-    const check = () => {
-        return new Promise<string | undefined>((resolve, reject) => {
-            const subId = unit.aggregator.ws.subscribe('ausf', {
-                payload: address,
-                callback: data => {
-                    if(data.kind === 'initial'){
-                        const isLiquidationOrder= data.orders?.find(o => o.pair === instrument && o.liquidated);
-                        if(isLiquidationOrder){
-                            unit.aggregator.ws.unsubscribe(subId);
-                            unit.aggregator.ws.destroy();
-                            resolve(isLiquidationOrder.id);
-                        } else {
-                            resolve(undefined)
-                        }
-                    }
-                }
-            })
-            setTimeout(() => reject(new Error('Timeout')), 5000);
-        })
-    }
-
-    return new Promise<string>(async (resolve, reject) => {
-        let orderId = await check();
-        while(orderId === undefined){
-            orderId = await check();
-        }
-        resolve(orderId);
-        setTimeout(() => reject(new Error('Timeout')), 30000);
-    })
-}
-
-const waitUntilPositionClosed = (address: string, instrument: string) => {
-    return new Promise<void>((resolve, reject) => {
-        const subId = unit.aggregator.ws.subscribe('ausf', {
-            payload: address,
-            callback: data => {
-                const position = data.balances?.find(p => p.instrument === instrument);
-                if (position && parseFloat(position.position) === 0){
-                    unit.aggregator.ws.unsubscribe(subId);
-                    unit.aggregator.ws.destroy();
-                    resolve();
-                }
+  const check = () => {
+    return new Promise<string | undefined>((resolve, reject) => {
+      const subId = unit.aggregator.ws.subscribe('ausf', {
+        payload: address,
+        callback: data => {
+          if (data.kind === 'initial') {
+            const isLiquidationOrder = data.orders?.find(o => o.pair === instrument && o.liquidated);
+            if (isLiquidationOrder) {
+              unit.aggregator.ws.unsubscribe(subId);
+              unit.aggregator.ws.destroy();
+              resolve(isLiquidationOrder.id);
+            } else {
+              resolve(undefined)
             }
-        })
-        setTimeout(() => reject(new Error('Timeout')), 30000);
+          }
+        }
+      })
+      setTimeout(() => {
+        reject(new Error('Liquidation order timeout'));
+      }, 5000);
     })
+  }
+
+  return new Promise<string>(async (resolve, reject) => {
+    setTimeout(() => { reject(new Error('Timeout')); }, 30000);
+
+    let orderId = await check();
+    while (orderId === undefined) {
+      orderId = await check();
+    }
+    resolve(orderId);
+  })
 }
 
+const waitUntilPositionZeroed = (address: string, instrument: string) => {
+  return new Promise<void>((resolve, reject) => {
+    const subId = unit.aggregator.ws.subscribe('ausf', {
+      payload: address,
+      callback: data => {
+        const position = data.balances?.find(p => p.instrument === instrument);
+        if (position && parseFloat(position.position) === 0) {
+          unit.aggregator.ws.unsubscribe(subId);
+          unit.aggregator.ws.destroy();
+          resolve();
+        }
+      }
+    })
+    setTimeout(() => { reject(new Error('Timeout')); }, 30000);
+  })
+}
 
 const { address } = await simpleFetch(unit.blockchainService.getCrossMarginInfo)();
-if(address == undefined) throw new TypeError("address is undefined");
-
+if (address === undefined) throw new TypeError('address is undefined');
 
 // Prerequisites:
 // 1. Wallet with USDT
 
 // Defining wallet
 const wallet = new ethers.Wallet(
-    walletPrivateKey,
-    unit.provider
+  PRIVATE_KEY,
+  unit.provider
 );
 const walletAddress = await wallet.getAddress();
 
@@ -214,48 +240,48 @@ const stopPrice = undefined; // optional
 
 const bnAmount = ethers.utils.parseUnits(DEPOSIT_AMOUNT, decimals);
 if (allowance.lt(bnAmount)) {
-    await collateralContract.approve(address, ethers.constants.MaxUint256); // Sometimes before approve you need to call approve(0)
+  await collateralContract.approve(address, ethers.constants.MaxUint256); // Sometimes before approve you need to call approve(0)
 }
 
 // 2. Make deposit to Cross CFD contract
 await crossMarginCFDContract.depositAsset(bnAmount);
 
-const {instruments} = await simpleFetch(unit.blockchainService.getCrossMarginInfo)();
+const { instruments } = await simpleFetch(unit.blockchainService.getCrossMarginInfo)();
 const instrumentInfo = instruments[instrument];
 if (!instrumentInfo) throw new Error(`Instrument not found for symbol ${instrument}`);
 
 const { buyPower, buyPrice, sellPower, sellPrice } = await getFuturesInfo(walletAddress, instrument, 0.1);
 const priceChange24h = await getPriceChange24h(instrument);
+console.log(`Price change 24h: ${priceChange24h > 0 ? '+' : ''}${priceChange24h}%`);
 
 // First order params
 const side: 'BUY' | 'SELL' = priceChange24h < 0 ? 'BUY' : 'SELL';
 const amount = side === 'BUY' ? buyPower : sellPower;
 const price = side === 'BUY' ? buyPrice : sellPrice;
-if (price == undefined) throw new TypeError(`Price is undefined. Expected number. Side: ${side}`);
+if (price === undefined) throw new TypeError(`Price is undefined. Expected number. Side: ${side}`);
 
-
-const {totalFee} = await unit.calculateFee(instrument, amount, 'cross');
-console.log(`Open position order: ${side} ${amount} ${instrument} at price ${price} with fee ${totalFee}`);
-const {matcherAddress} = await simpleFetch(unit.blockchainService.getInfo)();
+const { totalFee } = await unit.calculateFee(instrument, amount, 'cross');
+console.log(`Open position order: ${side} ${amount} ${instrument} at price ${price} with fee ${totalFee.toString()}`);
+const { matcherAddress } = await simpleFetch(unit.blockchainService.getInfo)();
 
 // Signing order
 const signedOrder = await crypt.signCrossMarginCFDOrder(
-    instrumentInfo.id, // instrumentIndex
-    side, // side: 'BUY' | 'SELL'
-    price,
-    amount,
-    totalFee,
-    walletAddress,
-    matcherAddress,
-    false, // usePersonalSign
-    wallet, // pass here ethers.Signer instance
-    unit.chainId,
-    stopPrice,
-    false // isFromDelegate — if true, then the order will be placed on behalf of the delegate
+  instrumentInfo.id, // instrumentIndex
+  side, // side: 'BUY' | 'SELL'
+  price,
+  amount,
+  totalFee,
+  walletAddress,
+  matcherAddress,
+  false, // usePersonalSign
+  wallet, // pass here ethers.Signer instance
+  unit.chainId,
+  stopPrice,
+  false // isFromDelegate — if true, then the order will be placed on behalf of the delegate
 );
 
 // 3. Make order: open position
-const {orderId} = await simpleFetch(unit.aggregator.placeCrossMarginOrder)(signedOrder);
+const { orderId } = await simpleFetch(unit.aggregator.placeCrossMarginOrder)(signedOrder);
 console.log(`Order ${orderId} placed`);
 
 // 4. Wait order until Settled status. Timeout 30 seconds.
@@ -272,6 +298,8 @@ const liquidationOrderId = await waitLiquidationOrder(walletAddress, instrument)
 
 // 8. Wait liquidation order until Settled status. Timeout 30 seconds.
 await waitOrderSettlement(walletAddress, liquidationOrderId);
+console.log(`Liquidation order ${liquidationOrderId} settled`);
 
-// 9. Check that position by instrument is disappeared.
-await waitUntilPositionClosed(walletAddress, instrument);
+// 9. Check that position by instrument is zeroed.
+await waitUntilPositionZeroed(walletAddress, instrument);
+console.log(`Position by instrument ${instrument} zeroed`);
