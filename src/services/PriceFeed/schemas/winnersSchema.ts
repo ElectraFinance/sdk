6d030920ed3fc23708dd714bbf323bc2d0cc0f53
Kirill Litvinov <kigastu@gmail.com>
2024-02-16 21:49:32 +0300

    update winnersSchema

--- src/services/PriceFeed/schemas/winnersSchema.ts
+++ src/services/PriceFeed/schemas/winnersSchema.ts
@@ -3,15 +3,17 @@ import { z } from 'zod';
 export const winnersSchema = z.object({
   bestTrade: z.object({
     address: z.string(),
-    asset: z.string(),
+    instrument: z.string(),
     leverage: z.number(),
-    pnl: z.number()
+    pnl: z.number(),
+    side: z.string()
   }),
   worstTrade: z.object({
     address: z.string(),
-    asset: z.string(),
+    instrument: z.string(),
     leverage: z.number(),
-    pnl: z.number()
+    pnl: z.number(),
+    side: z.string()
   }),
   bestPnl: z.object({ address: z.string(), pnl: z.number() }),
   worstPnl: z.object({ address: z.string(), pnl: z.number() })
