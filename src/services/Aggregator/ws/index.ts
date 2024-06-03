80db25a510e1c3098f49634804c603eb364155ac
Slava Startsev <slava@gton.capital>
2024-06-03 21:36:41 +0300

    chore: next bump

--- src/services/Aggregator/ws/index.ts
+++ src/services/Aggregator/ws/index.ts
@@ -385,7 +385,7 @@ class AggregatorWS {
 
         console.log('=== SDK ===', {
           subKey,
-          subscriptions: JSON.stringify(this.subscriptions),
+          ausf: JSON.stringify(this.subscriptions.ausf),
           prevSubscriptionId,
         });
       } else {
