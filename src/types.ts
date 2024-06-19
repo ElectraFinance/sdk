473c5c3d574c6aa8344e0e16be630460ca426a89
Slava Startsev <slavastartsev@orionprotocol.io>
2024-06-19 19:03:59 +0300

    Add event horizon testnet network (#74)
    * feat: add event horizon network
    
    * chore: bump v

--- src/types.ts
+++ src/types.ts
@@ -186,6 +186,7 @@ export enum SupportedChainId {
 
   BSC_TESTNET = '97',
   SEPOLIA = '11155111',
+  EVENT_HORIZON_TESTNET = '123420000034',
 
   // For testing and debug purpose
   // BROKEN = '0',
