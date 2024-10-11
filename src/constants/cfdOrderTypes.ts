b0c481677e9457d9bfcc1a580bb29aedcd880674
sunsetlover36 <sunsetloverrr36@gmail.com>
2024-10-11 15:37:03 +0300

    Update cfd order types

--- src/constants/cfdOrderTypes.ts
+++ src/constants/cfdOrderTypes.ts
@@ -22,5 +22,18 @@ export const CROSS_MARGIN_CFD_ORDER_TYPES = {
     { name: 'matcherFee', type: 'uint64' },
     { name: 'expiration', type: 'uint64' },
     { name: 'buySide', type: 'uint8' },
-  ]
+  ],
+  CFDOrderV2: [
+    { name: 'senderAddress', type: 'address' },
+    { name: 'price', type: 'uint80' },
+    { name: 'instrumentIndex', type: 'uint16' },
+    { name: 'matcherAddress', type: 'address' },
+    { name: 'price2', type: 'uint80' },
+    { name: 'buySide', type: 'uint8' },
+    { name: 'amount', type: 'uint96' },
+    { name: 'matcherFee', type: 'uint64' },
+    { name: 'expiration', type: 'uint64' },
+    { name: 'orderType', type: 'uint8' },
+    { name: 'signerChainId', type: 'uint64' },
+  ],
 };
