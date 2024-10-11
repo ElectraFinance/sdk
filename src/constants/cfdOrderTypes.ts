684bd5ecb0d0b2692932cce0bdee9d7def029682
sunsetlover36 <sunsetloverrr36@gmail.com>
2024-10-11 15:44:28 +0300

    Bump order types version to OrderV2

--- src/constants/cfdOrderTypes.ts
+++ src/constants/cfdOrderTypes.ts
@@ -11,6 +11,22 @@ export const ISOLATED_MARGIN_CFD_ORDER_TYPES = {
     { name: 'buySide', type: 'uint8' },
   ],
 };
+export const ISOLATED_MARGIN_CFD_ORDER_TYPES_V2 = {
+  CFDOrderV2: [
+    { name: 'senderAddress', type: 'address' },
+    { name: 'price', type: 'uint80' },
+    { name: 'instrumentIndex', type: 'uint16' },
+    { name: 'matcherAddress', type: 'address' },
+    { name: 'price2', type: 'uint80' },
+    { name: 'buySide', type: 'uint8' },
+    { name: 'amount', type: 'uint96' },
+    { name: 'matcherFee', type: 'uint64' },
+    { name: 'expiration', type: 'uint64' },
+    { name: 'orderType', type: 'uint8' },
+    { name: 'signerChainId', type: 'uint64' },
+    { name: 'nonce', type: 'uint64' },
+  ],
+};
 
 export const CROSS_MARGIN_CFD_ORDER_TYPES = {
   CFDOrder: [
@@ -23,6 +39,8 @@ export const CROSS_MARGIN_CFD_ORDER_TYPES = {
     { name: 'expiration', type: 'uint64' },
     { name: 'buySide', type: 'uint8' },
   ],
+};
+export const CROSS_MARGIN_CFD_ORDER_TYPES_V2 = {
   CFDOrderV2: [
     { name: 'senderAddress', type: 'address' },
     { name: 'price', type: 'uint80' },
