dbfbf3e25bf9e04cd01a1f73fbe011ed2f511da7
sunsetlover36 <sunsetloverrr36@gmail.com>
2024-12-04 17:31:50 +0300

    Add new chains

--- src/services/BlockchainService/schemas/getNetworkFeeSchema.ts
+++ src/services/BlockchainService/schemas/getNetworkFeeSchema.ts
@@ -2,7 +2,13 @@ import { z } from 'zod';
 import { SupportedChainId } from '../../../types';
 
 const getNetworkFeeSchema = z.record(
-  z.nativeEnum(SupportedChainId).transform((val) => val.toString()),
+  z.union([
+    z.nativeEnum(SupportedChainId).transform((val) => val.toString()),
+
+    z.literal('3'),
+    z.literal('5'),
+    z.literal('80001'),
+  ]),
   z.string()
 );
 
