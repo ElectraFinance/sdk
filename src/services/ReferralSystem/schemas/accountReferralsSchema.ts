cea4ce5978e0296fabe7807f8503510b2a922c30
Hayk <Poghosyan Hayk>
2025-02-06 15:56:48 +0400

    feat: added getUserTMAPoints

--- src/services/ReferralSystem/schemas/accountReferralsSchema.ts
+++ src/services/ReferralSystem/schemas/accountReferralsSchema.ts
@@ -1,18 +1,24 @@
-import { z } from 'zod';
-import { paginationInfoSchema } from './paginationInfoSchema';
+import { z } from "zod";
+import { paginationInfoSchema } from "./paginationInfoSchema";
 
 const dataSchema = z.object({
   referral: z.string(),
   relative_level: z.number(),
   volume: z.number(),
-  points: z.number()
-})
+  points: z.number(),
+});
 
 export const accountReferralsSchema = z.object({
   data: z.array(dataSchema),
-  pagination_info: paginationInfoSchema
-})
+  pagination_info: paginationInfoSchema,
+});
 
-export const bitgetReferralsSchema = z.array(z.string())
+export const bitgetReferralsSchema = z.array(z.string());
+
+export const tmaUserPoints = z.object({
+  status: z.string(),
+  points: z.string(),
+  status_of_claim: z.string(),
+});
 
 export type AccountReferrals = z.infer<typeof accountReferralsSchema>;
